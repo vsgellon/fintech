50ec02cf7aad5a65268eb5360740e85431500b39
vsgellon <96087202+vsgellon@users.noreply.github.com>
2022-01-09 22:20:27 -0800

    part 2 complete

--- github_upload/loan_analyzer.py
+++ github_upload/loan_analyzer.py
@@ -63,21 +63,28 @@ loan = {
 
 # @TODO: Use get() on the dictionary of additional information to extract the Future Value and Remaining Months on the loan.
 # Print each variable.
-# YOUR CODE HERE!
+future_value = loan.get("future_value")
+remaining_months = loan.get("remaining_months")
+print("This is the future value: $", future_value, "and this is the remaining months:", remaining_months)
+
+
 
 
 # @TODO: Use the formula for Present Value to calculate a "fair value" of the loan.
 # Use a minimum required return of 20% as the discount rate.
 #   You'll want to use the **monthly** version of the present value formula.
 #   HINT: Present Value = Future Value / (1 + Discount_Rate/12) ** remaining_months
+discount_rate = .20
+present_value = future_value / (1 + discount_rate/12) ** remaining_months
 
-# YOUR CODE HERE!
 
 # If Present Value represents what the loan is really worth, does it make sense to buy the loan at its cost?
 # @TODO: Write a conditional statement (an if-else statement) to decide if the present value represents the loan's fair value.
 #    If the present value of the loan is greater than or equal to the cost, then print a message that says the loan is worth at least the cost to buy it.
 #    Else, the present value of the loan is less than the loan cost, then print a message that says that the loan is too expensive and not worth the price.
-# YOUR CODE HERE!
+if present_value > loan.get("loan_price"):
+    print("Don't sell this loan because it is worth more than the loan price today")
+
 
 
 """Part 3: Perform Financial Calculations.
